8440ff5a089fbfc2d90281fa192e9215af7e2598
gtm510 <108852243+gtm510@users.noreply.github.com>
2023-01-05 17:45:08 +0900

    Update slick.js

--- js/slick.js
+++ js/slick.js
@@ -12,3 +12,27 @@
         centerPadding: '50px',
         focusOnSelect: true,
     });
+
+$(document).ready(function(){
+        // スクロールで反応するアニメーション
+        $(window).on('load scroll', function (){
+            SetScrollTiming('.ani');
+        });
+    });
+ 
+    // スクロールのタイミングでアニメーションさせる
+    function SetScrollTiming(target){
+        var box = $(target);
+        var animated = 'animated';
+ 
+        box.each(function(){
+            var boxOffset = $(this).offset().top;
+            var scrollPos = $(window).scrollTop();
+            var wh = $(window).height();
+ 
+            //画面内のどの位置で処理を実行するかで「100」の値を変更
+            if(scrollPos > boxOffset - wh + 100 ){
+                $(this).addClass('move');
+            }
+        });
+    }
